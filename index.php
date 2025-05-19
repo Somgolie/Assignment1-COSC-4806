59459612af3eb524f56c41a97369132629b7404a
Somgolie <sejinkeonye@algomau.ca>
2025-05-19 21:40:36 +0000

    added a login page

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
+ <!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello EveryNya!</p>'; ?> 
-</html>
+<head>
+<title>Home</title>
+</head>
+<body>
+
+<h1>Assignment 1</h1>
+<p><a href='./login.php'>Log in Here!</a></p>
+
+</body>
+</html> 
---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Log in</title>
+</head>
+<body>
+
+<h1>Log in to your account</h1>
+<form action="/validate.php" method="post">
+  <label for="fname">Username:</label><br>
+  <input type="text" id="username" name="username" ><br>
+  <label for="lname">Password:</label><br>
+  <input type="password" id="password" name="password" ><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html> 
